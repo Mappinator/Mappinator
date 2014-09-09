ed19b2bff62f8928cd203d62d7a5a35c9b5fdc0e
IWillBeWithUForever <1091664831@qq.com>
2014-09-09 21:19:44 +0800

    sth terrible added

--- Test.php
+++ Test.php
@@ -1,5 +1,9 @@
 <div style="width:100%;height:100%;position:absolute;font-size:100%;background-color:red;font-size:100%">
-<div style="font-size:300%;"> 123 </div>
+<form action="" style="width:50%;height:50%;font-size:100%;">
+<input id="username" type="text" style="width:50%;height:50%;font-size:100%;" value="username"></input>
+<input id="password" type="password" style="width:50%;height:50%;font-size:100%;" value="password"></input>
+<input id="submit" type="button" style="width:50%;height:50%;font-size:100%;" value="submit"></input>
+</form>
 <div id="2" class="link" style="left:25%;top:25%;width:50%;height:50%;position:absolute;font-size:50%;">
 </div>
 </div>
